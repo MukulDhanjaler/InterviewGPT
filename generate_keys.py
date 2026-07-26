8b3353b8765e8872e07919aac900eb142e35506a
Mukul Dhanjal <158339511+MukulDhanjaler@users.noreply.github.com>
2026-07-26 17:24:32 +0530

    Update user credentials in generate_keys.py

--- generate_keys.py
+++ generate_keys.py
@@ -3,9 +3,9 @@ from pathlib import Path
 
 import streamlit_authenticator as stauth
 
-names = ["snehit", "vaddi", "test"]
-usernames = ["snehit", "vaddi", "Test"]
-passwords = ["snehit", "snehit", "test"]
+names = ["mukul", "vaddi", "test"]
+usernames = ["mukul", "vaddi", "Test"]
+passwords = ["mukul", "snehit", "test"]
 
 hashed_passwords = stauth.Hasher(passwords).generate()
 
